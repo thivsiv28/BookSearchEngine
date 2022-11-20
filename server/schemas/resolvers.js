d32f1d0be3d4525d8343c846b8fe7272b4b8ad01
Thiviya Sivasithamparam <thiviya.sivasithamparam@gmail.com>
2022-11-20 00:42:04 -0500

    Basic login

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,36 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find({});
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      debugger;
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        // throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
