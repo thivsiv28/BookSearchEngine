a33298d2d93e60e3ed9c4e651783676570623a05
Thiviya Sivasithamparam <thiviya.sivasithamparam@gmail.com>
2022-11-20 21:28:54 -0500

    specify node version to 16

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import { loginUser } from "../utils/API";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
